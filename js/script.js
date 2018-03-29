dc2e2753e63304a87d8981d48ef24745003cfcd9
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-03-29 22:15:23 +0200

    Init project and add script in script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Podaj  zmianną a: ');
+var h = prompt('Podaj zmienna h: ');
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a:' + a + ' and height h:' + h + ' is equal to:' + triangleArea);
